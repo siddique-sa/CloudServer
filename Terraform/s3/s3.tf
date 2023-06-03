5071b4a7311d92cbdbfd4c63f5f7a5e9cf402c53
siddique-sa <96865065+siddique-sa@users.noreply.github.com>
2023-06-03 14:36:06 +0000

    rm s3 bck conf

--- Terraform/s3/s3.tf
+++ Terraform/s3/s3.tf
@@ -2,10 +2,6 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-terraform {
-  backend "s3" {}
-}
-
 resource "aws_s3_bucket" "mys3" {
   bucket = "self-state-storage"
   acl    = "public"
@@ -15,15 +11,5 @@ resource "aws_s3_bucket" "mys3" {
   }
   tags = {
     Name = "My self-state-storage Bucket"
-  }
-}
-
-# Configure backend S3 bucket after its creation
-data "terraform_remote_state" "backend_config" {
-  backend = "s3"
-  config = {
-    bucket = aws_s3_bucket.mys3.id
-    key    = "s3/terraform.tfstate"
-    region = "ap-south-1"  
   }
 }
